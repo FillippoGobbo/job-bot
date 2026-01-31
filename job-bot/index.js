4a1e28d4ce4c9b8a4a1c216205e57e57c0f0d542
filippo gobbo <filippo@Mac.lan>
2026-01-31 23:46:38 +0100

    Aggiornato selettore bottone per Playwright

---
+++ job-bot/index.js
@@ -0,0 +1,31 @@
+console.log("🤖 Bot avviato correttamente");
+
+setInterval(() => {
+  console.log("Bot online e in esecuzione...");
+}, 60000);
+
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch({ headless: true }); // invisibile
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // Vai al sito
+  await page.goto("https://carphub.it");
+
+  // Aspetta che il bottone sia visibile
+  await page.waitForSelector(
+    "#root > div.min-h-screen.bg-background > header > div > div > div > a > button",
+    { timeout: 5000 },
+  );
+
+  // Clicca il bottone
+  await page.click(
+    "#root > div.min-h-screen.bg-background > header > div > div > div > a > button",
+  );
+
+  console.log("Bottone cliccato!");
+
+  await browser.close();
+})();
